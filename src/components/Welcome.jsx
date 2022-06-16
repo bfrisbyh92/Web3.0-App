c45d6336ebc35bf00ac30a74d71ca26b34caeb97
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-06-16 13:19:57 -0400

    welcome component done for now, moving to loader component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -28,6 +28,10 @@ const Welcome = () => {
 
   }
 
+  const handleSubmit = () => {
+
+  }
+
  
   return (
     <div className="flex w-full justify-center items-center">
@@ -108,6 +112,21 @@ const Welcome = () => {
 
                 <Input placeholder="Enter Message" name="message"
                 type="text" handleChange={() => {}}/>
+                
+                <div className="h-[1px] w-full bg-gray-400 my-2"/>
+
+                {/*isLoading*/ /*true */ false ? (
+                    <Loader/>
+                ) : (
+                  <button
+                  type="button"
+                   onClick={handleSubmit}
+                   className="text-white w-full mt-2 border-[1px] p-2 
+                   border-[#3d4f7c] rounded-full cursor-pointer"
+                  >
+                  Send Now
+                  </button>
+                )}
             </div>
 
         </div>
